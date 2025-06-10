768279f7bb2c706a9ff616d29cc3c75c7b615890
mcadams150 <ericosborne97@gmail.com>
2025-06-10 17:52:39 -0400

    TESTING

--- main.py
+++ main.py
@@ -3,15 +3,132 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 #define your specific motor pwm pins here
-motor1pin = 9
-motor2pin = 2
-motor3pin = 3
-motor4pin = 4
-motor5pin = 5
-motor6pin = 6
-motor7pin = 7
-motor8pin = 8
+motor_driver_1_ena = 3
+motor_driver_1_int_1 = 5
+motor_driver_1_int_2 = 7
+motor_driver_1_int_3 = 11
+motor_driver_1_int_4 = 13
+motor_driver_1_enb = 15
 
-GPIO.setup(motor1pin, GPIO.OUT)
-#done
-GPIO.PWM(motor1pin, 50)
+motor_driver_2_ena = 19
+motor_driver_2_int_1 = 21
+motor_driver_2_int_2 = 23
+motor_driver_2_int_3 = 29
+motor_driver_2_int_4 = 31
+motor_driver_2_enb = 33
+
+motor_driver_3_ena = 8
+motor_driver_3_int_1 = 10
+motor_driver_3_int_2 = 12
+motor_driver_3_int_3 = 22
+motor_driver_3_int_4 = 24
+motor_driver_3_enb = 26
+
+motor_driver_4_ena = 36
+motor_driver_4_int_1 = 38
+motor_driver_4_int_2 = 40
+motor_driver_4_int_3 = 29
+motor_driver_4_int_4 = 31
+motor_driver_4_enb = 32
+
+pin_to_gpio = {
+    3: 2,
+    5: 3,
+    7: 4,
+    8: 14,
+    10: 15,
+    11: 17,
+    12: 18,
+    13: 27,
+    15: 22,
+    16: 23,
+    18: 24,
+    19: 10,
+    21: 9,
+    22: 25,
+    23: 11,
+    24: 8,
+    26: 7,
+    29: 5,
+    31: 6,
+    32: 12,
+    33: 13,
+    35: 19,
+    36: 16,
+    37: 26,
+    38: 20,
+    40: 21
+}
+
+def board_pin_to_gpio(pin):
+    return pin_to_gpio.get(pin, None)
+
+#Convert from pin number to GPIO number
+motor_driver_1_ena = board_pin_to_gpio(motor_driver_1_ena)
+motor_driver_1_int_1 = board_pin_to_gpio(motor_driver_1_int_1)
+motor_driver_1_int_2 = board_pin_to_gpio(motor_driver_1_int_2)
+motor_driver_1_int_3 = board_pin_to_gpio(motor_driver_1_int_3)
+motor_driver_1_int_4 = board_pin_to_gpio(motor_driver_1_int_4)
+motor_driver_1_enb = board_pin_to_gpio(motor_driver_1_enb)
+
+motor_driver_2_ena = board_pin_to_gpio(motor_driver_2_ena)
+motor_driver_2_int_1 = board_pin_to_gpio(motor_driver_2_int_1)
+motor_driver_2_int_2 = board_pin_to_gpio(motor_driver_2_int_2)
+motor_driver_2_int_3 = board_pin_to_gpio(motor_driver_2_int_3)
+motor_driver_2_int_4 = board_pin_to_gpio(motor_driver_2_int_4)
+motor_driver_2_enb = board_pin_to_gpio(motor_driver_2_enb)
+
+motor_driver_3_ena = board_pin_to_gpio(motor_driver_3_ena)
+motor_driver_3_int_1 = board_pin_to_gpio(motor_driver_3_int_1)
+motor_driver_3_int_2 = board_pin_to_gpio(motor_driver_3_int_2)
+motor_driver_3_int_3 = board_pin_to_gpio(motor_driver_3_int_3)
+motor_driver_3_int_4 = board_pin_to_gpio(motor_driver_3_int_4)
+motor_driver_3_enb = board_pin_to_gpio(motor_driver_3_enb)
+
+motor_driver_4_ena = board_pin_to_gpio(motor_driver_4_ena)
+motor_driver_4_int_1 = board_pin_to_gpio(motor_driver_4_int_1)
+motor_driver_4_int_2 = board_pin_to_gpio(motor_driver_4_int_2)
+motor_driver_4_int_3 = board_pin_to_gpio(motor_driver_4_int_3)
+motor_driver_4_int_4 = board_pin_to_gpio(motor_driver_4_int_4)
+motor_driver_4_enb = board_pin_to_gpio(motor_driver_4_enb)
+
+
+#Set mode and all pins to output
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(motor_driver_1_ena, GPIO.OUT)
+GPIO.setup(motor_driver_1_int_1, GPIO.OUT)
+GPIO.setup(motor_driver_1_int_2, GPIO.OUT)
+GPIO.setup(motor_driver_1_int_3, GPIO.OUT)
+GPIO.setup(motor_driver_1_int_4, GPIO.OUT)
+GPIO.setup(motor_driver_1_enb, GPIO.OUT)
+
+GPIO.setup(motor_driver_2_ena, GPIO.OUT)
+GPIO.setup(motor_driver_2_int_1, GPIO.OUT)
+GPIO.setup(motor_driver_2_int_2, GPIO.OUT)
+GPIO.setup(motor_driver_2_int_3, GPIO.OUT)
+GPIO.setup(motor_driver_2_int_4, GPIO.OUT)
+GPIO.setup(motor_driver_2_enb, GPIO.OUT)
+
+GPIO.setup(motor_driver_3_ena, GPIO.OUT)
+GPIO.setup(motor_driver_3_int_1, GPIO.OUT)
+GPIO.setup(motor_driver_3_int_2, GPIO.OUT)
+GPIO.setup(motor_driver_3_int_3, GPIO.OUT)
+GPIO.setup(motor_driver_3_int_4, GPIO.OUT)
+GPIO.setup(motor_driver_3_enb, GPIO.OUT)
+
+GPIO.setup(motor_driver_4_ena, GPIO.OUT)
+GPIO.setup(motor_driver_4_int_1, GPIO.OUT)
+GPIO.setup(motor_driver_4_int_2, GPIO.OUT)
+GPIO.setup(motor_driver_4_int_3, GPIO.OUT)
+GPIO.setup(motor_driver_4_int_4, GPIO.OUT)
+GPIO.setup(motor_driver_4_enb, GPIO.OUT)
+
+#Configure all motors to run at 100 pwm
+
+speed = 100
+
+motor_driver_1_ena_pwm = GPIO.PWM(motor_driver_1_ena, speed)
+motor_driver_1_ena_pwm.start(0)
+GPIO.output(motor_driver_1_int_1, GPIO.HIGH)
+GPIO.output(motor_driver_1_int_3, GPIO.HIGH)
