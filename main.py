804d1b978899cc8242b69976fb65523e0bbd1fb3
mcadams150 <ericosborne97@gmail.com>
2025-06-10 19:39:53 -0400

    TESTING

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ tire_2_steering_direction_2 = motor_driver_2_int_2
 
 
 #Set mode and all pins to output
-
+'''
 GPIO.setup(motor_driver_1_ena, GPIO.OUT)
 GPIO.setup(motor_driver_1_int_1, GPIO.OUT)
 GPIO.setup(motor_driver_1_int_2, GPIO.OUT)
@@ -119,7 +119,6 @@ GPIO.setup(motor_driver_1_int_3, GPIO.OUT)
 GPIO.setup(motor_driver_1_int_4, GPIO.OUT)
 GPIO.setup(motor_driver_1_enb, GPIO.OUT)
 
-'''
 GPIO.setup(motor_driver_2_ena, GPIO.OUT)
 GPIO.setup(motor_driver_2_int_1, GPIO.OUT)
 GPIO.setup(motor_driver_2_int_2, GPIO.OUT)
