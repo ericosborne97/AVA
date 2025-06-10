3510356d6ac7df1311c25471d530bbd24250a6fd
mcadams150 <ericosborne97@gmail.com>
2025-06-10 18:54:27 -0400

    TESTING

--- main.py
+++ main.py
@@ -155,8 +155,8 @@ tire_2_steering_enable_pwm = GPIO.PWM(tire_2_steering_enable, speed)
 tire_2_rotation_enable_pwm = GPIO.PWM(tire_2_rotation_enable, speed)
 
 while True:
-    tire_2_rotation_enable_pwm.start(speed)
-    GPIO.output(tire_2_rotation_direction_1, GPIO.LOW)
-    GPIO.output(tire_1_rotation_direction_2, GPIO.HIGH)
+    tire_2_steering_enable_pwm.start(speed)
+    GPIO.output(tire_2_steering_direction_1, GPIO.LOW)
+    GPIO.output(tire_1_steering_direction_2, GPIO.HIGH)
 
 GPIO.cleanup()
